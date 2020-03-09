1b6f0664c12ddb03cda7ff0b8df0505ee52c3d01
ongjh <48179219+ongjh@users.noreply.github.com>
2020-03-09 14:49:17 +0800

    first commit

--- app/src/main/java/com/example/toolbarexample/MainActivity.kt
+++ app/src/main/java/com/example/toolbarexample/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.toolbarexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        // Handle action bar item clicks here.
+//        val id = item.getItemId()
+//
+//        if (id == R.id.action_one) {
+//            Toast.makeText(this, "Item One Clicked", Toast.LENGTH_LONG).show()
+//            return true
+//        }
+//        if (id == R.id.action_two) {
+//            Toast.makeText(this, "Item Two Clicked", Toast.LENGTH_LONG).show()
+//            return true
+//        }
+//        if (id == R.id.action_three) {
+//            Toast.makeText(this, "Item Three Clicked", Toast.LENGTH_LONG).show()
+//            return true
+//        }
+//
+//        return super.onOptionsItemSelected(item)
+//
+//    }
 }
